edaabbf18e5abf71edba58d6c79718da41ab2e25
erick <erickjhorman@gmail.com>
2021-05-31 17:17:45 -0500

    i implemented the logic of Event-Driven Arquitecture

--- notification-service/src/main/java/com/programming/techie/notificationservice/NotificationServiceApplication.java
+++ notification-service/src/main/java/com/programming/techie/notificationservice/NotificationServiceApplication.java
@@ -1,13 +1,22 @@
 package com.programming.techie.notificationservice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+import java.util.function.Consumer;
 
 @SpringBootApplication
 public class NotificationServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NotificationServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationServiceApplication.class, args);
+    }
 
+    @Bean
+    public Consumer<String> notificationEventSupplier() {
+        return message -> new EmailSender().sendEmail(message);
+    }
 }
